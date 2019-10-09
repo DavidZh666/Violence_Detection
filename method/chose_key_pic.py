1d0da79022a8b7ab3f1038265fa7e7fd213a813c
David Zh <38425439+DavidZh666@users.noreply.github.com>
2019-10-10 01:05:59 +0800

    Create chose_key_pic.py

---
+++ method/chose_key_pic.py
@@ -0,0 +1,17 @@
+import cv2
+import os
+
+row=480
+col=270
+
+block_row=48  # 48
+block_col=45  # 45
+
+stride_row=14  # 7,8
+stride_col=16  #
+
+def videokey(path):  # video path
+  pass # wait completed 10.10
+
+if __name__ == "__main__":
+    main()
